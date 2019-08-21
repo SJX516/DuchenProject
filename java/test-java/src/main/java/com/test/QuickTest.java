f130a541dd3b4eb283961487023e3ab57d42e06c
Duchen <516199576@qq.com>
2019-08-22 01:56:30 +0800

    update gradle

---
+++ java/test-java/src/main/java/com/test/QuickTest.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+public class QuickTest {
+
+    public static void main(String[] args) {
+        Integer[] data = new Integer[]{5,2,1,3,9,10,22,4};
+        String s = "123";
+        String[] a = null;
+        Object o;
+        log(s.replace(s.charAt(0)+"", ""));
+        log((int)s.charAt(0) + "");
+        log((char)49 + "");
+    }
+
+    public static void log(String s) {
+        System.out.println(s);
+    }
+}
